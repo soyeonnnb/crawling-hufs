c3f562ecb45d35f74f9102fdb65ff021e1ea1733
soyeonnnb <la28s5d@naver.com>
2021-08-19 18:41:24 +0900

    rename crawler and start apps for templates

--- q_and_a/apps.py
+++ q_and_a/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class CrawlerConfig(AppConfig):
+class QAndAConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'crawler'
+    name = 'q_and_a'
